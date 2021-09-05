a033077a929d7980b9d203635eac4260ae9202a8
雷瀚琳 <zlx8wan@outlook.com>
2021-09-05 23:52:26 +0800

    添加项目文件。

---
+++ XmalTempleteFinder/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+using DevExpress.Xpf.Docking;
+using DevExpress.Xpf.Prism;
+
+using Prism.Ioc;
+using Prism.Regions;
+using Prism.Unity;
+
+using System.Windows;
+
+using XmalTempleteFinder.Views;
+
+namespace XmalTempleteFinder
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<TempleteContent>();
+        }
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            var factory = Container.Resolve<IRegionBehaviorFactory>();
+            var re = DevExpress.Xpf.Prism.AdapterFactory.Make<RegionAdapterBase<DocumentGroup>>(factory);
+            regionAdapterMappings.RegisterMapping(typeof(DocumentGroup), re);
+        }
+    }
+}
---
+++ XmalTempleteFinder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace XmalTempleteFinder.ViewModels
+{
+   public class MainWindowViewModel:BindableBase
+    {
+        private ObservableCollection<Object> controlsSource;
+
+        public ObservableCollection<Object> ControlsSource
+        {
+            get { return controlsSource; }
+            set => SetProperty(ref controlsSource, value);
+        }
+       public ICommand LoadedCommand { set; get; }
+        IRegionManager _regionManager;
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            LoadedCommand = new DelegateCommand(()=> {
+                _regionManager.RequestNavigate("MainRegion", "TempleteContent");
+            });
+        }
+    }
+}
